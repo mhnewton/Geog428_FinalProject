48f6493477a9d31e7562414626f3a2ad7b2c1f41
Mike Newton <58487446+mhnewton@users.noreply.github.com>
2020-04-02 16:54:16 -0700

    Update ScatterplotCode.R

--- ScatterplotCode.R
+++ ScatterplotCode.R
@@ -19,7 +19,6 @@ head(firePoly)
 firePoly$SIZE_HA <- log(firePoly$SIZE_HA + 1) #add 1 so values less than 1 are not negative
 firePoly$Near.totalPrecip <- log(firePoly$Near.totalPrecip + 1)
 firePoly$Interpolate.AvePrecip <- log(firePoly$Interpolate.AvePrecip + 1)
-firePoly$Near.AveTemp <- (firePoly$Near.AveTemp)
 #head(firePoly)
 
 #linear models
@@ -49,21 +48,14 @@ plot(lm.model.IntPrecip)
 png("./NearTempScatterplot.png", width = 8, height = 6, units = "in", res = 300) #create a png file
 ggplot(firePoly, aes(x=Near.AveTemp, y=SIZE_HA)) + #classify x and y, qualitative classes
   geom_point(size = 0.5) + #set point size
-  stat_smooth(data = firePoly, aes(x=Near.AveTemp, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x) +
-  labs(title = "Average Monthly Temperature vs. fire size in British Columbia 2017\nUsing Find Nearest tool", x = "Monthly Temperature (°C)", y = "Fire Size (log(hectares)",
-       caption = "Figure 1: Scatterplot of average monthly temperature and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
-  theme_classic() + #set the theme to classic (removes background and borders etc.)
-  theme(plot.title = element_text(face = "bold", hjust = 0.5), plot.caption = element_text(hjust = 0)) #set title to center and bold
-dev.off()
-
-#scatterplot near tool Precip
-png("./NearPrecipScatterplot.png", width = 8, height = 6, units = "in", res = 300) #create a png file
-ggplot(firePoly, aes(x=Near.totalPrecip, y=SIZE_HA)) + #classify x and y, qualitative classes
-  geom_point(size = 0.5) + #set point size
-  stat_smooth(data = firePoly, aes(x=Near.totalPrecip, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x) +
-  abline(lm.model.NearPrecip, lwd = 2, col = "blue")+
-  labs(title = "Monthly Precipitation vs. fire size in British Columbia 2017\nUsing Find Nearest tool", x = "Monthly Total Precipitation (log(mm))", y = "Fire Size (log(hectares)",
-       caption = "Figure 2: Scatterplot of monthly total precipitation and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
+  annotate(geom="text", x=5, y=10, label=paste("Y = ", signif(lm.model.NearTemp$coef[[2]],3),"X + ", signif(lm.model.NearTemp$coef[[2]],3))) +
+  annotate(geom="text", x=5, y=9, label=paste("Adj R2 = ",signif(summary(lm.model.NearTemp)$adj.r.squared, 3))) +
+  annotate(geom="text", x=5, y=8, label=paste("P =",signif(summary(lm.model.NearTemp)$coef[2,4],3))) +
+  stat_smooth(data = firePoly, aes(x=Near.AveTemp, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x, col = "red") +
+  labs(title = "Average Monthly Temperature vs. fire size in British Columbia 2017\nUsing Find Nearest tool", 
+       x = "Monthly Temperature (°C)", 
+       y = "Fire Size (log(hectares)",
+       caption = "Figure 2: Scatterplot of average monthly temperature and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
   theme_classic() + #set the theme to classic (removes background and borders etc.)
   theme(plot.title = element_text(face = "bold", hjust = 0.5), plot.caption = element_text(hjust = 0)) #set title to center and bold
 dev.off()
@@ -72,10 +64,30 @@ dev.off()
 png("./InterpolateTempScatterplot.png", width = 8, height = 6, units = "in", res = 300) #create a png file
 ggplot(firePoly, aes(x=Interpolate.AveTemp, y=SIZE_HA)) + #classify x and y, qualitative classes
   geom_point(size = 0.5) + #set point size
-  stat_smooth(data = firePoly, aes(x=Near.AveTemp, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x) +
-  abline(lm.model.IntTemp, lwd = 2, col = "red")+
-  labs(title = "Average Monthly Temperature vs. fire size in British Columbia 2017\nUsing Interpolation and Zonal Statistics", x = "Monthly Temperature (°C)", y = "Fire Size (log(hectares)",
+  annotate(geom="text", x=5, y=10, label=paste("Y = ", signif(lm.model.IntTemp$coef[[2]],3),"X + ", signif(lm.model.NearTemp$coef[[2]],3))) +
+  annotate(geom="text", x=5, y=9, label=paste("Adj R2 = ",signif(summary(lm.model.IntTemp)$adj.r.squared, 3))) +
+  annotate(geom="text", x=5, y=8, label=paste("P =",signif(summary(lm.model.IntTemp)$coef[2,4],3))) +
+  stat_smooth(data = firePoly, aes(x=Near.AveTemp, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x, col = "red") +
+  labs(title = "Average Monthly Temperature vs. fire size in British Columbia 2017\nUsing Interpolation and Zonal Statistics", 
+       x = "Monthly Temperature (°C)", 
+       y = "Fire Size (log(hectares)",
        caption = "Figure 3: Scatterplot of average monthly temperature and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
+  theme_classic() + #set the theme to classic (removes background and borders etc.)
+  theme(plot.title = element_text(face = "bold", hjust = 0.5), plot.caption = element_text(hjust = 0)) #set title to center and bold
+dev.off()
+
+#scatterplot near tool Precip
+png("./NearPrecipScatterplot.png", width = 8, height = 6, units = "in", res = 300) #create a png file
+ggplot(firePoly, aes(x=Near.totalPrecip, y=SIZE_HA)) + #classify x and y, qualitative classes
+  geom_point(size = 0.5) + #set point size
+  annotate(geom="text", x=5, y=10, label=paste("Y = ", signif(lm.model.NearPrecip$coef[[2]],3),"X + ", signif(lm.model.NearTemp$coef[[2]],3))) +
+  annotate(geom="text", x=5, y=9, label=paste("Adj R2 = ",signif(summary(lm.model.NearPrecip)$adj.r.squared, 3))) +
+  annotate(geom="text", x=5, y=8, label=paste("P =",signif(summary(lm.model.NearPrecip)$coef[2,4],3))) +
+  stat_smooth(data = firePoly, aes(x=Near.totalPrecip, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x, col = "blue") +
+  labs(title = "Monthly Precipitation vs. fire size in British Columbia 2017\nUsing Find Nearest tool", 
+       x = "Monthly Total Precipitation (log(mm))", 
+       y = "Fire Size (log(hectares)",
+       caption = "Figure 4: Scatterplot of monthly total precipitation and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
   theme_classic() + #set the theme to classic (removes background and borders etc.)
   theme(plot.title = element_text(face = "bold", hjust = 0.5), plot.caption = element_text(hjust = 0)) #set title to center and bold
 dev.off()
@@ -84,10 +96,14 @@ dev.off()
 png("./InterpolatePrecipScatterplot.png", width = 8, height = 6, units = "in", res = 300) #create a png file
 ggplot(firePoly, aes(x=Interpolate.AvePrecip, y=SIZE_HA)) + #classify x and y, qualitative classes
   geom_point(size = 0.5) + #set point size
-  stat_smooth(data = firePoly, aes(x=Near.totalPrecip, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x) +
-  abline(lm.model.IntPrecip, lwd = 2, col = "blue")+
-  labs(title = "Average Monthly Precipitation vs. fire size in British Columbia 2017\nUsing Interpolation and Zonal Statistics", x = "Monthly Total Precipitation (log(mm))", y = "Fire Size (log(hectares))",
-       caption = "Figure 4: Scatterplot of average monthly temperature and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
+  annotate(geom="text", x=5, y=10, label=paste("Y = ", signif(lm.model.IntPrecip$coef[[2]],3),"X + ", signif(lm.model.NearTemp$coef[[2]],3))) +
+  annotate(geom="text", x=5, y=9, label=paste("Adj R2 = ",signif(summary(lm.model.IntPrecip)$adj.r.squared, 3))) +
+  annotate(geom="text", x=5, y=8, label=paste("P =",signif(summary(lm.model.IntPrecip)$coef[2,4],3))) +
+  stat_smooth(data = firePoly, aes(x=Near.totalPrecip, y=SIZE_HA), method = lm, se = FALSE, formula = y ~ x, col = "blue") +
+  labs(title = "Average Monthly Precipitation vs. fire size in British Columbia 2017\nUsing Interpolation and Zonal Statistics", 
+       x = "Monthly Total Precipitation (log(mm))", 
+       y = "Fire Size (log(hectares))",
+       caption = "Figure 5: Scatterplot of average monthly temperature and fire size of fire incidents during 2017 in British Columbia.") + #label plot, x axis, y axis
   theme_classic() + #set the theme to classic (removes background and borders etc.)
   theme(plot.title = element_text(face = "bold", hjust = 0.5), plot.caption = element_text(hjust = 0)) #set title to center and bold
 dev.off()
